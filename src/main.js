d0c96a57f410fb58bf52a0306b3273b8fc082830
Murat Corlu <muratcorlu@me.com>
2021-02-01 01:17:38 +0100

    iOS Safari workaround

--- src/main.js
+++ src/main.js
@@ -20,51 +20,21 @@ document.getElementById('filepicker').addEventListener('change', (el) => {
 
         domvas.toImage(document.getElementById('preview'), function () {
             context.drawImage(this, 0, 0);
-            // document.getElementById('download').href = canvas.toDataURL('image/png');
+
+            document.getElementById('result').setAttribute('src', canvas.toDataURL('image/png'));
         });
-
-
-        // domtoimage.toSvg(document.getElementById('preview'))
-        //     .then(function (result) {
-        //         console.log(result);
-
-        //         document.getElementById('download').href = result;
-        //     });
-        // var svg = document.querySelector('svg');
-        // var data = new XMLSerializer().serializeToString(svg);
-        // var win = window.URL || window.webkitURL || window;
-        // var img = new Image();
-        // var blob = new Blob([data], { type: 'image/svg+xml' });
-        // var url = win.createObjectURL(blob);
-        // img.onload = function () {
-        //     context.drawImage(img, 0, 0);
-        //     win.revokeObjectURL(url);
-        //     // var uri = canvas.toDataURL('image/png').replace('image/png', 'octet/stream');
-        //     // var a = document.createElement('a');
-        //     // document.body.appendChild(a);
-        //     // a.style = 'display: none';
-        //     // a.href = uri
-        //     // a.download = (svg.id || svg.svg.getAttribute('name') || svg.getAttribute('aria-label') || 'untitled') + '.png';
-        //     // a.click();
-        //     // window.URL.revokeObjectURL(uri);
-        //     // document.body.removeChild(a);
-        // };
-        // img.src = url;
     };
 
     reader.readAsDataURL(el.target.files[0])
 });
 
 document.getElementById('download').addEventListener('click', () => {
+    container.classList.remove('step-2');
+    container.classList.add('step-3');
+
     canvas.toBlob(function(blob) {
         saveAs(blob, "ch-avatar.png");
     }, 'image/png');
-    // domtoimage.toSvg(document.getElementById('preview'))
-    //     .then(function (result) {
-    //         console.log(result);
-    //         // window.saveAs(result, 'my-ch-avatar.png');
-    //     })
-    //     .catch((err) => console.error(err));
 });
 
 
