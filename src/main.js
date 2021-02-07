79528569407aed8f9dd6bdac78b5a6796a1561aa
Murat Corlu <muratcorlu@me.com>
2021-02-07 17:16:12 +0100

    generating image without dom, just svg

--- src/main.js
+++ src/main.js
@@ -13,16 +13,34 @@ if (navigator.canShare && navigator.canShare()) {
 document.getElementById('filepicker').addEventListener('change', (el) => {
     var reader = new FileReader();
     reader.onload = function (e) {
-        document.getElementById('preview').setAttribute('src', e.target.result);
+        document.getElementById('preview').setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', e.target.result);
 
         container.classList.remove('step-1');
         container.classList.add('step-2');
 
-        domvas.toImage(document.getElementById('preview'), function () {
-            context.drawImage(this, 0, 0);
+        var svg = document.querySelector('svg');
+        var data = new XMLSerializer().serializeToString(svg);
+        var win = window.URL || window.webkitURL || window;
+        var img = new Image();
+        var blob = new Blob([data], { type: 'image/svg+xml' });
+        var url = win.createObjectURL(blob);
+        img.onload = function () {
+            console.log(img);
 
-            document.getElementById('result').setAttribute('src', canvas.toDataURL('image/png'));
-        });
+            context.drawImage(img, 0, 0);
+            win.revokeObjectURL(url);
+            var uri = canvas.toDataURL('image/png');
+            document.getElementById('downloadLink').setAttribute('href', uri);
+            // var a = document.createElement('a');
+            // document.body.appendChild(a);
+            // a.style = 'display: none';
+            // a.href = uri
+            // a.download = 'ch-avatar.png';
+            // a.click();
+            // window.URL.revokeObjectURL(uri);
+            // document.body.removeChild(a);
+        };
+        img.src = url;
     };
 
     reader.readAsDataURL(el.target.files[0])
